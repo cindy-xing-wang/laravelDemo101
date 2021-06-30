e84b307476e8e909f16e14c06d45151de0949963
Cindy Wang <cindywang010@live.cn>
2021-07-01 09:13:57 +1200

    change projectName

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/calendars', function () {
-    return view('checklistLog.index');
-});
 
-Route::resource('staffs', StaffController::class);
-Route::resource('checklists', ChecklistController::class);
 Route::group(['middleware'=>['auth','admin']], function(){
+    Route::resource('staffs', StaffController::class);
+    Route::resource('checklists', ChecklistController::class);
 });
 
 Route::group(['middleware'=>['auth']], function(){
     Route::get('/dashboard', function () {
         return view('dashboard');
     });
-    
+    Route::get('/calendars', function () {
+        return view('checklistLog.index');
+    });
     Route::resource('checklistLogs', ChecklistLogController::class);
     Route::post('/checklistLogs/check', [ChecklistLogController::class, 'check'])->name('checklistLog.check');
     Route::get('/export-excel/{id}', [ChecklistLogController::class, 'exportIntoExcel']);
